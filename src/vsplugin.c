3fb34e361637c8027b77f1186bd41f1fd262af76
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-03-02 19:08:08 +0800

    cosmetics

--- src/vsplugin.c
+++ src/vsplugin.c
@@ -26,8 +26,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include </usr/local/include/vapoursynth/VapourSynth4.h>
-#include </usr/local/include/vapoursynth/VSHelper4.h>
+#include <vapoursynth/VapourSynth4.h>
+#include <vapoursynth/VSHelper4.h>
 #include "descale.h"
 #include "plugin.h"
 #include <stdio.h>
